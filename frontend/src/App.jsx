ac1581dd4184955cd4fc1c0f977adcfe90ad0ca6
dalia202 <s121150@stu.najah.edu>
2026-05-17 00:22:26 +0300

    Complete doctor frontend and backend module

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,9 @@ import AdminDashboard from "./pages/Admin-dashboard";
 import AddClinic from "./pages/Admin-addClinic";
 
 import { Routes, Route } from "react-router-dom";
-
+import AdminManageDoctors from "./pages/Admin-manageDoctors";
+import EditPersonalInfo from "./pages/EditPersonalInfo";
+import EditDoctor from "./pages/EditDoctor";
 function App() {
 
   const [count, setCount] = useState(0)
@@ -15,8 +17,11 @@ function App() {
       <Route path="/" element={<AdminDashboard />} />
       <Route path="/manage-clinics" element={<AdminManageClinics />} />
       <Route path="/add-clinic/:id?" element={<AddClinic />} />
+      <Route path="/manage-doctors" element={<AdminManageDoctors />} />
+      <Route path="/profile/edit" element={<EditPersonalInfo />} />
+      <Route path="/manage-doctors/edit/:id" element={<EditDoctor />} />
     </Routes>
-  );
-}
+  );};
+
 
 export default App;
